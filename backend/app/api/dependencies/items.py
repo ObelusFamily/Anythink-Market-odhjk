63f9505e9e40a9c7f8111a50e2225f6edd656563
felix.larrue <felixlarrue@gmail.com>
2022-07-30 16:15:02 +0200

    add filters on item title

--- backend/app/api/dependencies/items.py
+++ backend/app/api/dependencies/items.py
@@ -22,11 +22,13 @@ def get_items_filters(
     tag: Optional[str] = None,
     seller: Optional[str] = None,
     favorited: Optional[str] = None,
+    title: Optional[str] = None,
     limit: int = Query(DEFAULT_ITEMS_LIMIT, ge=1),
     offset: int = Query(DEFAULT_ITEMS_OFFSET, ge=0),
 ) -> ItemsFilters:
     return ItemsFilters(
         tag=tag,
+        title=title,
         seller=seller,
         favorited=favorited,
         limit=limit,
